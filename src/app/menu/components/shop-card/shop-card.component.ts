e2c4b58604603b2899346d766077a050bb1682bf
Ali Hosseinkhani <aliho3einkhani1@gmail.com>
2024-06-12 13:39:50 +0330

    [wip] shop-card imp

--- src/app/menu/components/shop-card/shop-card.component.ts
+++ src/app/menu/components/shop-card/shop-card.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuApi } from '../../shared/menu.api';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { BehaviorSubject } from 'rxjs';
+import { ShopCard } from '../../../shared/model/shop-card.model';
 
 @Component({
   selector: 'app-shop-card',
@@ -8,9 +10,12 @@ import { ActivatedRoute } from '@angular/router';
   styleUrl: './shop-card.component.scss'
 })
 export class ShopCardComponent implements OnInit {
+  private _cards$ = new BehaviorSubject<ShopCard[]>([]);
+  public get cards(): ShopCard[] { return this._cards$.getValue() }
 
   constructor(
     private route: ActivatedRoute,
+    private router: Router,
     private menuApi: MenuApi
   ) { }
 
@@ -20,7 +25,26 @@ export class ShopCardComponent implements OnInit {
 
   getData() {
     const { userId } = this.route.snapshot.params;
-    this.menuApi.getUserShopCard(userId).subscribe()
+    this.menuApi.getUserShopCard(userId).subscribe(({ data, success }: any) => {
+      const cards = data.card.map((el: any) => {
+        return {
+          ...el,
+          inCardAmount: 1
+        }
+      })
+      if (success) this._cards$.next(cards);
+    })
   }
 
+  minCount(card: ShopCard) {
+    card.inCardAmount -= 1;
+  }
+
+  addCount(card: ShopCard) {
+    card.inCardAmount += 1;
+  }
+
+  navigateToProduct(card: ShopCard) {
+    this.router.navigate(['menu/products', card.categoryId, card.productId])
+  }
 }
---
+++ src/app/shared/model/shop-card.model.ts
@@ -0,0 +1,9 @@
+export interface ShopCard {
+    amount: number;
+    categoryId: number;
+    price: number;
+    productId: number;
+    imageUrl: string;
+    title: string;
+    inCardAmount: number;
+}
