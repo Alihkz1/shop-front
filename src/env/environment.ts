f62baabfee62463e041fa0a24d49a6b5ae8014c2
Ali Hosseinkhani <aliho3einkhani1@gmail.com>
2024-06-24 09:09:44 +0330

    refactor

--- src/env/environment.ts
+++ src/env/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    API_BASE: "http://151.245.238.125:8081/api/",
+    API_BASE: "http://localhost:8081/api/",
     UPLOAD_URL: "https://api.cloudinary.com/v1_1/dxivgzpks/image/upload"
 }
