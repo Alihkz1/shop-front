521d1298735e10f9c0cd7cb2c0cf51e2050e7147
Ali Hosseinkhani <aliho3einkhani1@gmail.com>
2024-07-19 14:02:55 +0330

    refactor loading

--- src/app/shared/component/loading/loading.component.ts
+++ src/app/shared/component/loading/loading.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
-import { NzSpinModule } from 'ng-zorro-antd/spin';
 
 @Component({
   selector: 'loading',
   standalone: true,
-  imports: [NzSpinModule],
   templateUrl: './loading.component.html',
   styleUrl: './loading.component.scss'
 })
---
+++ src/app/shared/component/snow/snow.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-snow',
+  templateUrl: './snow.component.html',
+  styleUrl: './snow.component.scss'
+})
+export class SnowComponent {
+
+}
